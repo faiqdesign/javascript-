e48e2418edd33e30853c1f87f08c4154dd9e2bcb
faiqdesign <161251301+faiqdesign@users.noreply.github.com>
2024-04-18 14:36:45 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+var str ="faiq malik";
+var a =str.length;
+
+console.log(a)
+
+
+var str ="faiq malik";
+var a =str.toLocaleLowerCase();
+
+console.log(a)
+
+var str ="faiq malik";
+var a =str.toUpperCase();
+
+console.log(a)
+
+
+var str ="faiq malik";
+var a =str.startsWith("faiq");
+
+console.log(a)
+
+
+var str ="faiq malik";
+var a =str.endsWith("maliK");
+console.log(a)
+
+var str ="faiq malik";
+var a =str.indexOf("q");
+
+console.log(a)
+
+
+var str ="faiq malik";
+var a =str.lastIndexOf("l");
+
+console.log(a)
+
+
+
+
+    var str ="faiq malik";
+    var a =str.replace("malik","ahsan");
+
+console.log(a)
+
+
+var str ="    faiq        ";
+
+alert(str);
+
+
+var str ="    faiq        ";
+var a = str.trim();
+alert(a);
+
+
+
+
+var str ="faiq malik";
+var a =str.charAt(9);
+
+console.log(a)
